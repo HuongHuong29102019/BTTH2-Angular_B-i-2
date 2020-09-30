09f9a899f64a78f3c5c571a702929f3e2029f363
HuongHuong29102019 <70678478+HuongHuong29102019@users.noreply.github.com>
2020-09-30 16:15:07 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BTTH2tiep';
+  constructor(private renderer: Renderer2) {
+  }
+  ngOnInit() {  
+  }
+  ngAfterViewInit() { 
+    this.loadScripts();
+  }
+  public loadScripts() {
+    this.renderExternalScript('assets/js/main.js').onload = () => {
+    }
+  }
+public renderExternalScript(src: string): HTMLScriptElement {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.async = true;
+    script.defer = true;
+    this.renderer.appendChild(document.body, script);
+    return script;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AppComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
